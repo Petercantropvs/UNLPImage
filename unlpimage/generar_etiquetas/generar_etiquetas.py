35c5a46322dc4057d4e68346242019fa1e26657c
Petercantropvs <pedrogvillegas@gmail.com>
2023-05-04 18:11:39 -0300

    Primer estructura

---
+++ unlpimage/generar_etiquetas/generar_etiquetas.py
@@ -0,0 +1,55 @@
+# Etiquetas
+
+import PySimpleGUI as sg
+import sys
+import os
+
+treedata = sg.TreeData()
+photo_path = os.path.join('C:\\','Users','Usuario','Documents','Pedro','Origami')
+
+# Función para construir el treedata (carpetas, subcarpetas y archivos)
+def arbol(parent, directorio):
+    folder_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABnUlEQVQ4y8WSv2rUQRSFv7vZgJFFsQg2EkWb4AvEJ8hqKVilSmFn3iNvIAp21oIW9haihBRKiqwElMVsIJjNrprsOr/5dyzml3UhEQIWHhjmcpn7zblw4B9lJ8Xag9mlmQb3AJzX3tOX8Tngzg349q7t5xcfzpKGhOFHnjx+9qLTzW8wsmFTL2Gzk7Y2O/k9kCbtwUZbV+Zvo8Md3PALrjoiqsKSR9ljpAJpwOsNtlfXfRvoNU8Arr/NsVo0ry5z4dZN5hoGqEzYDChBOoKwS/vSq0XW3y5NAI/uN1cvLqzQur4MCpBGEEd1PQDfQ74HYR+LfeQOAOYAmgAmbly+dgfid5CHPIKqC74L8RDyGPIYy7+QQjFWa7ICsQ8SpB/IfcJSDVMAJUwJkYDMNOEPIBxA/gnuMyYPijXAI3lMse7FGnIKsIuqrxgRSeXOoYZUCI8pIKW/OHA7kD2YYcpAKgM5ABXk4qSsdJaDOMCsgTIYAlL5TQFTyUIZDmev0N/bnwqnylEBQS45UKnHx/lUlFvA3fo+jwR8ALb47/oNma38cuqiJ9AAAAAASUVORK5CYII='
+    file_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABU0lEQVQ4y52TzStEURiHn/ecc6XG54JSdlMkNhYWsiILS0lsJaUsLW2Mv8CfIDtr2VtbY4GUEvmIZnKbZsY977Uwt2HcyW1+dTZvt6fn9557BGB+aaNQKBR2ifkbgWR+cX13ubO1svz++niVTA1ArDHDg91UahHFsMxbKWycYsjze4muTsP64vT43v7hSf/A0FgdjQPQWAmco68nB+T+SFSqNUQgcIbN1bn8Z3RwvL22MAvcu8TACFgrpMVZ4aUYcn77BMDkxGgemAGOHIBXxRjBWZMKoCPA2h6qEUSRR2MF6GxUUMUaIUgBCNTnAcm3H2G5YQfgvccYIXAtDH7FoKq/AaqKlbrBj2trFVXfBPAea4SOIIsBeN9kkCwxsNkAqRWy7+B7Z00G3xVc2wZeMSI4S7sVYkSk5Z/4PyBWROqvox3A28PN2cjUwinQC9QyckKALxj4kv2auK0xAAAAAElFTkSuQmCC'
+
+    for archivo in os.listdir(directorio):
+        ruta = os.path.join(directorio, archivo)
+        
+        if os.path.isdir(ruta):    # Si es carpeta
+            treedata.Insert(parent, ruta, archivo, values=[], icon=folder_icon)
+            arbol(ruta, ruta)
+        else:                         # Acá el values hay que completarlo con los tags
+            treedata.Insert(parent, ruta, archivo, values=[os.stat(ruta).st_size], icon=file_icon)
+
+def layout():
+    arbol('',photo_path)
+    frame_layout = [[sg.Text('Nombre archivo')], [sg.Image('perro.png')],[sg.Text('Tags:')],[sg.Text('', key = '-OUTPUT-')]]
+
+    layout_l = sg.Column([[sg.Tree(data = treedata, headings = ['Tags',], auto_size_columns=True,
+                    expand_x = True, expand_y = True, enable_events = True)],
+                        [sg.Text('Tag')],
+                        [sg.Input(size=(50,1), key = '-TAG-'),sg.Button('Agregar')],
+                        [sg.Text('Texto descriptivo')],
+                        [sg.Input(size=(50,1), key = '-ALGO-'),sg.Button('Agregar')]])
+
+    layout_r = sg.Column([[sg.Push(), sg.Button('Volver', key = '-VOLVER-')],
+                        [sg.Frame('Imagen seleccionada.jpg(?',
+                                    frame_layout, 
+                                    background_color=sg.theme_button_color()[1], 
+                                    size=(300,400))]])                      
+    layout = [[sg.Text('Seleccione la imagen que desee etiquetar:')],[layout_l, layout_r], [sg.Push(), sg.Button('Guardar')]]
+    return layout
+
+def ventana_etiquetas():
+    window = sg.Window('Etiquetar imágenes', layout(), resizable=True, finalize=True)
+    while True:
+        event, values = window.read()
+        window['-OUTPUT-'].update(values['-TAG-'])
+        print(event, values)
+        if event == sg.WIN_CLOSED or event == '-VOLVER-':
+            break
+            
+    window.close()
+
+if __name__ == '__main__':
+    ventana_etiquetas()
---
+++ unlpimage/unlpimage.py
@@ -0,0 +1,5 @@
+from generar_etiquetas import generar_etiquetas
+import PySimpleGUI as sg
+
+print('Hola Peco!')
+generar_etiquetas.ventana_etiquetas()
